eda7f3914374b8364564e35e1c2d3850fc54fc38
Andy Fitzgerald <andy.fitzgerald@dbs.ie>
2024-03-04 14:49:13 +0000

    chore: razor syntax example

---
+++ ECommerceMVC/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ECommerceMVC.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public double? Price {get; set;}
+};
