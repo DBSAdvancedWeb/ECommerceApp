0eab4fc0675b6c55a8c4863e377e72a4c6547cce
Andy Fitzgerald <andy.fitzgerald@dbs.ie>
2024-03-14 11:03:21 +0000

    chore: controller tutorial

--- ECommerceMVC/Controllers/HomeController.cs
+++ ECommerceMVC/Controllers/HomeController.cs
@@ -15,12 +15,29 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> Products = GetProductsList();
+        _logger.LogInformation($"Getting list of products: {Products.Count()} in total");
+        return View(Products);
     }
 
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    private IEnumerable<Product> GetProductsList() {
+        return new List<Product>()
+        {
+            new Product { Id = 1, Name = "Fitness Trackers", Description = "Aliexpress fintness trackers", ImageUrl = "imgs/aliexpress-fitness-trackers.jpg", Price = 89.99 },
+            new Product { Id = 2, Name = "Black Bag", Description = "Black over the shoulder bag", ImageUrl = "imgs/black-bag-over-the-shoulder.jpg", Price = 59.95 },
+            new Product { Id = 2, Name = "Runners", Description = "Black Sneakers with white sole", ImageUrl = "imgs/black-sneakers-with-white-sole.jpg", Price = 40.00 },
+            new Product { Id = 1, Name = "Headphones", Description = "Volume Control Headphones", ImageUrl = "imgs/volume-control-headphones.jpg", Price = 179.99 },
+            new Product { Id = 2, Name = "Sport Runners", Description = "All black sports runners", ImageUrl = "imgs/right-foot-all-black-sneaker.jpg", Price = 44.99 },
+            new Product { Id = 2, Name = "QR Codes", Description = "QR Codes In Store", ImageUrl = "imgs/qr-codes-in-store.jpg", Price = 2.99 },
+            new Product { Id = 1, Name = "Gemstone Necklace", Description = "Purple Gemstone necklace", ImageUrl = "imgs/purple-gemstone-necklace.jpg", Price = 19.95 },
+            new Product { Id = 2, Name = "Modern Watch", Description = "Modern time piece watch", ImageUrl = "imgs/modern-time-pieces.jpg", Price = 45.00 },
+            new Product { Id = 2, Name = "Leather Jacket", Description = "Kids Leather Jacket", ImageUrl = "imgs/kids-leather-jacket.jpg", Price = 119.99 }
+        };
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
